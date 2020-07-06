dc0314160d65af87260ba8b2d45ac942036844fc
ruganga <ulanruganga@gmail.com>
2020-07-06 21:55:46 +0600

    add chat and messages with redux

--- client/components/chat.js
+++ client/components/chat.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addMessage } from '../redux/reducers/infobase'
 
 const Chat = () => {
+
+  const dispatch = useDispatch()
+  const status = useSelector((s) => s.infobase.status)
+  const [value, setValue] = useState()
+
 
   return (
     <div className="w-full border shadow">
@@ -8,7 +15,7 @@ const Chat = () => {
         <div className="w-full flex flex-col">
           <div className="border-b flex px-6 py-2 items-center">
             <div className="flex flex-col">
-              <h3 className="text-grey-darkest text-md mb-1 font-extrabold">#general</h3>
+              <h3 className="text-grey-darkest text-md mb-1 font-extrabold">#{status}</h3>
               <div className="text-grey font-thin text-sm">
                 Chit-chattin about ugly HTML and mixing of concerns.
               </div>
@@ -30,7 +37,18 @@ const Chat = () => {
           </div>
           <div className="flex m-6 rounded-lg border-2 border-grey overflow-hidden">
             <span className="text-3xl text-grey px-3 border-r-2 border-grey">+</span>
-            <input type="text" className="w-full px-4" placeholder="Message to #general" />
+            <input
+              type="text"
+              className="w-full px-4"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              placeholder={`Message to #${status}`}
+            />
+            <button
+              type="button"
+              onClick={(() => dispatch(addMessage(status, 'username', value, +new Date)))}>
+              ok
+            </button>
           </div>
         </div>
       </div>
